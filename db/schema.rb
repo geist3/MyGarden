f7c62329a596271dacafc8426b3c9e19e6dc5cfc
jcockrell <jcockrell@gmail.com>
2011-04-25 18:28:53 +0100

    add variety to plant

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110425105411) do
+ActiveRecord::Schema.define(:version => 20110425172221) do
 
   create_table "logentries", :force => true do |t|
     t.string   "notes"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20110425105411) do
     t.string   "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "variety"
   end
 
 end
